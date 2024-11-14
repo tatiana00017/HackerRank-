4ec8b36b82efc980ef68d841637517cdc5cfc9b2
Tatiana Campos <71901494+tatiana00017@users.noreply.github.com>
2024-11-14 09:09:49 -0500

    Add AVeryBigSum.java

---
+++ java/AVeryBigSum.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+    public static long aVeryBigSum(List<Long> ar) {
+        long sum=0;
+        for (int i = 0; i < ar.size(); i++) {
+            sum +=ar.get(i);
+        }
+        return sum;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Long> ar = new ArrayList<>();
+
+        for (int i = 0; i < arCount; i++) {
+            long arItem = Long.parseLong(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        long result = Result.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
