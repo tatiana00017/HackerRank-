2b0b4659cdf53bd4440350b75d9eb7ba33dfc1a4
Tatiana Campos <71901494+tatiana00017@users.noreply.github.com>
2024-11-21 22:43:45 -0500

    Add GradingStudents.java

---
+++ java/GradingStudents.java
@@ -0,0 +1,58 @@
+// https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        for (int i = 0; i < grades.size(); i++) {
+            int a=(grades.get(i)/5);
+            int b=((a+1)*5)-grades.get(i);
+            if (b<3 && grades.get(i)>37) {
+                grades.set(i, (a+1)*5);
+            }
+        }
+        return grades;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
